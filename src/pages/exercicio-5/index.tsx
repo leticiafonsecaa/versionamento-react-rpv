abd42a836f82eedabd19b6d8672288d7c9d4d11b
leticiafonsecaa <fonleticia09@gmail.com>
2026-04-15 21:20:13 -0300

    adcionar exercicio 5

---
+++ src/pages/exercicio-5/index.tsx
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react';
+
+export default function Ex05() {
+    const [users, setUsers] = useState<string[]>([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => setUsers(data.map((user: {name: string}) => user.name)))
+    }, []);
+
+    return(
+        <div className="p-8 flex flex-col gap-4">
+            <h1>Exercicio 05</h1>
+            <p>Aqui você vai fazer o Fetch de Usuários</p>
+            <ul>
+
+                {users.map((user, index) => (
+                    <li className="p-2 content rounded-md border border-zinc-300" key={index}>{user}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
