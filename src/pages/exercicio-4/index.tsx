44e8a985e8fbc7924e256161da42404c855abe27
leticiafonsecaa <fonleticia09@gmail.com>
2026-04-15 21:10:41 -0300

    adcionando exercicio 4

---
+++ src/pages/exercicio-4/index.tsx
@@ -0,0 +1,49 @@
+import {useState} from 'react';
+
+export default function Ex04() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    function handleCadastro () {
+        if (email === '' || senha === '') {
+            alert('Preencha todos os campos');
+            return;
+        } 
+    
+        if (!email.includes('@') || !email.includes('.')) {
+            alert('Email inválido');
+            return;
+        }
+        if (senha.length < 6) {
+            alert('Senha muito curta');
+            return;
+        }
+        alert('Cadastro feito!');
+
+    }
+    
+        return(
+        <div className="p-8 flex flex-col gap-4">
+            <h1>Exercicio 04</h1>
+            <p>Aqui tem um Formulário de Cadastro</p>
+
+            <input
+            type= 'text'
+            placeholder= 'Email'
+            value= {email}
+            onChange= {(e) => setEmail(e.target.value)}
+            />
+
+            <input
+            type= 'password'
+            placeholder= 'Senha'
+            value= {senha}
+            onChange= {(e) => setSenha(e.target.value)}
+            />
+
+            <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded" onClick={handleCadastro}>
+            Cadastrar
+            </button>
+        </div>
+    );
+}
